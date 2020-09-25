f3e224124b547fbf9f1d1095e6d82b0dec6bc911
Ofir Ezra <Ofir561@gmail.com>
2020-09-25 19:50:23 +0300

    changed the name

--- jsonTable.js
+++ jsonTable.js
@@ -1,32 +1,32 @@
 const fs = require('fs')
 
-class Table {
+class JsonTable {
     constructor(path) {
         this.Path = path;
         this.File = undefined;
     }
 }
 
-Table.prototype.ReadTable = function() {
+JsonTable.prototype.ReadTable = function() {
     this.File = JSON.parse(fs.readFileSync(this.Path, 'utf8'));
 }
 
-Table.prototype.UpdateTable = function() {
+JsonTable.prototype.UpdateTable = function() {
     fs.writeFile(this.Path, JSON.stringify(this.File), (err) => {
         if(err) message.channel.send("There seems to be a problem with this request")
     })
 }
 
-Table.prototype.Write = function(key, value) {
+JsonTable.prototype.Write = function(key, value) {
     this.File[key] = value;
 }
 
-Table.prototype.Read = function(key) {
+JsonTable.prototype.Read = function(key) {
     return this.File[key];
 }
 
-Table.prototype.Delete = function(key) {
+JsonTable.prototype.Delete = function(key) {
     delete this.File[key];
 }
 
-module.exports = Table;
+module.exports = JsonTable;

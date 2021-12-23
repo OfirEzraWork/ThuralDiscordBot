49c0c4e7fb729ca635749d9289795633327234d7
OfirEzra <ofir.ezra@agritask.com>
2021-12-23 14:49:39 +0200

    removed several unnecessary blocks

--- index.js
+++ index.js
@@ -3,11 +3,7 @@ const { Client } = require("discord.js");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
-const MessageController = require("./controllers/messageController.js");
-
 const facadeController = require("./controllers/facadeController.js");
-
-const joinServerChannel = process.env.NEWPLAYERCHANNEL;
 
 const client = new Client({
   disableEveryone: true,
@@ -23,16 +19,6 @@ client.on("ready", () => {
 });
 client.on("message", async (message) => {
   facadeController.IncomingMessage(message);
-});
-client.on("guildMemberAdd", (member) => {
-  console.log("a player has joined the server");
-  member.guild.channels.cache
-    .get(joinServerChannel)
-    .send(
-      `${member.user.username} the brave has joined the quest!\\nMay his path lead to wealth and not an inevitable doom.`
-    );
-  let role = member.guild.roles.find("name", "Player");
-  member.addRole(role);
 });
 
 client.login(process.env.TOKEN);
